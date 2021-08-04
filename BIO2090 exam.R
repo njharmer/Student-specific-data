f166a7fb3437594202b169c733b8ffdfe1cfb0d9
njharmer <60611719+njharmer@users.noreply.github.com>
2021-08-04 17:18:24 +0100

    Add files via upload

---
+++ BIO2090 exam.R
@@ -0,0 +1,148 @@
+# Script to generate unique assignments for BIO2090 exam
+# Should provide each student with unique data and markers with an answer sheet.
+
+# Load packages
+library(ggplot2)
+library(officer)
+library(magrittr)
+setwd("C:/Users/njh209/OneDrive - University of Exeter/Nic's documents/R/resources")
+
+# Load student numbers
+Students = c("C12345", "C23456")
+
+# Set errors
+error <- c(15, 7)
+
+#Actions for each individual dataset
+
+for (j in 1:length(Students)) {
+
+  # Generate Bradford data
+  Bradford <- data.frame(Tube = c(1:6),
+                         Volume_of_BSA_solution_uL = c(0,4,8,12,16,20),
+                         Volume_of_water_uL = c(1000,996,992,988,984,980),
+                         Absorbance_595nm = c(0),
+                         concentration = c(0),
+                         Corrected_Absorbance_595nm = c(0)) 
+  grad <- 0.0025+(sample(0:100,1)/100000)
+  back <- 0.9+(sample(0:200,1)/1000)
+  
+  for (i in 1:6) {
+    Bradford[i,4] <- round(((100-error[2]+((sample(0:(error[2]*200),1))/100))/100)*(back+(Bradford[i,2]*10
+                                    *grad*((100-error[1]+((sample(0:(error[1]*200),1))/100))/100))), digits = 3)
+    Bradford[i,5] <- Bradford[i,2]*10
+    Bradford[i,6] <- Bradford[i,4]-Bradford[1,4]
+  }  
+  
+  # Calculate linear regression of Bradford
+  Brlr <- lm(Bradford[,6]~Bradford[,5])
+  slope <- as.numeric(Brlr$coefficients[2])
+  yint <- as.numeric(Brlr$coefficients[1])
+
+  # Generate table of results for unknowns
+  Fractions <- data.frame(Fraction = c("Homogenate, H", "Cytosol, C", "Mitochondria, M", "Nuclear, N",
+                                       "30% ammonium sulfate", "50% ammonium sulfate", "80% ammonium sulfate"),
+                          Volume_of_sample_uL = c(5),
+                          Volume_of_water_uL = c(996),
+                          Absorbance_595nm = c((back+0.35+sample(0:200,1)/1000), 
+                                               (back+0.5+sample(0:300,1)/1000),
+                                               (back+0.4+sample(0:400,1)/1000),
+                                               (back+0.25+sample(0:300,1)/1000),
+                                               (back+0.3+sample(0:50,1)/1000),
+                                               (back+0.5+sample(0:200,1)/1000),
+                                               (back+0.3+sample(0:100,1)/1000)),
+                          Corrected_Absorbance = c(0),
+                          x_value = c(0),
+                          concentration = c(0))
+  
+  for (i in 1:7) {
+    Fractions[i,5] <- Fractions[i,4]-Bradford[1,4]
+    Fractions[i,6] <- signif((Fractions[i,5]-yint)/slope,4)
+    Fractions[i,7] <- signif(Fractions[i,6]/5,3)
+  }
+  
+  # Generate PGK data
+  PGK <- data.frame(Fraction = c("Homogenate, H", "Cytosol, C", "Mitochondria, M", "Nuclear, N",
+                                 "30% ammonium sulfate", "50% ammonium sulfate", "80% ammonium sulfate"),
+                    Gradient_of_slope_AU_per_min = c((-0.03-sample(0:200,1)/1000), 
+                                         (-0.03-sample(0:200,1)/1000),
+                                         (sample(0:100,1)/-1000),
+                                         (-0.1-sample(0:100,1)/1000),
+                                         (-0.03-sample(0:20,1)/1000),
+                                         (-0.03-sample(0:30,1)/1000),
+                                         (-0.15-sample(0:200,1)/1000)),
+                    Product_concentration = c(0),
+                    Activity = c(0),
+                    Mass_protein = c(0),
+                    Specific_activity = c(0))
+  
+  for (i in 1:7) {
+    PGK[i,3] <- signif(PGK[i,2]*1000000/-6220,5)
+    PGK[i,4] <- PGK[i,3]/1000
+    PGK[i,5] <- Fractions[i,7]*0.005
+    PGK[i,6] <- signif(PGK[i,4]/PGK[i,5],3)
+  }
+    
+  # Generate student files
+
+  readout <- read_docx()
+  readout <- readout %>%
+    body_add_par("BIO2090 2020/21 January Examination", style = 'heading 1') %>%
+    body_add_par("") %>%
+    body_add_par(paste0("Individual data for student: ", Students[j]," for Question 1")) %>%
+    body_add_par("") %>%
+    body_add_par("Bradford standard curve data:", style = "Normal") %>%
+    body_add_par("") %>% 
+    body_add_table(value = Bradford[,1:4], style="table_template") %>%
+    body_add_par("") %>%
+    body_add_par("") %>%
+    body_add_par("Sample Bradford data:") %>%
+    body_add_par("") %>%
+    body_add_table(value = Fractions[,1:4], style="table_template") %>%
+    body_add_par("") %>%
+    body_add_par("") %>%
+    body_add_par("PGK assay data:") %>%
+    body_add_par("") %>%
+    body_add_table(value = PGK[,1:2], style="table_template") %>%
+    
+
+  print(readout, target = paste0("Student_files/", Students[j], ".docx"))
+
+  # Prepare figures
+  se <- ggplot(data = Bradford, aes(x = concentration, y=Corrected_Absorbance_595nm)) + 
+    stat_smooth(method = "lm",  formula = y~x, level = 0.75)
+  se <- se + geom_point(mapping = aes(size = 2)) + labs(y="Absorbance / AU", x="concentration / ug/mL")
+
+  # Generate lecturer files
+  
+  readout <- read_docx()
+  readout <- readout %>%
+    body_add_par("BIO2090 2020/21 Exam Dataset", style = 'heading 1') %>%
+    body_add_par("") %>%
+    body_add_par(paste0("Data and answers for student: ", Students[j])) %>%
+    body_add_par("") %>%
+    body_add_par("") %>%
+    body_add_par("Bradford standard curve data:", style = "Normal") %>%
+    body_add_par("") %>% 
+    body_add_table(value = Bradford, style="table_template") %>%
+    body_add_par("") %>%
+    body_add_par("") %>%
+    body_add_par(paste0("Bradford graph: formula is y = ", signif(slope,3),"x + ",signif(yint,3))) %>%
+    body_add_par("") %>%
+    body_add_gg(value = se, style = "centered") %>%
+    body_add_par("") %>%
+    body_add_par("") %>%
+    body_add_par("Sample Bradford data:") %>%
+    body_add_par("") %>%
+    body_add_table(value = Fractions, style="table_template") %>%
+    body_add_par("") %>%
+    body_add_par("") %>%
+    body_add_par("PGK assay data:") %>%
+    body_add_par("") %>%
+    body_add_table(value = PGK, style="table_template") %>%
+    
+
+  print(readout, target = paste0("Lecturer_files/", Students[j], "_answers.docx"))
+
+}
+
---
+++ BIO2090 practical assignment.R
@@ -0,0 +1,155 @@
+# Script to generate unique assignments for BIO2090 practical
+# Should provide each student with unique data and markers with an answer sheet.
+
+# Load packages
+library(ggplot2)
+library(officer)
+library(magrittr)
+setwd("C:/Users/njh209/OneDrive - University of Exeter/Nic's documents/R/resources")
+
+# Load student numbers
+Students = c(123456,234567)
+
+# Set up the protein datasets
+MW = c(66000, 60000, 17, 12, 13, 14, 20, 11)
+names <- c("BSA", "catalase", "myoglobin", "cytochrome c (horse)",  "ribonuclease A",
+           "a-lactalbumin", "GrpE", "cytochrome c (C. jejuni)")
+A410 <- c(0,1,3,3,0,0,0,3)
+protset <- matrix(data = c(1,3,6,2,3,6,1,4,6,1,4,7,1,3,8,1,5,8,2,3,8,2,5,7), nrow = 3, ncol = 8)
+
+# Set up table variables
+headers <- c("Fractions", "A280", "A410")
+
+#Actions for each individual dataset
+
+for (j in 1:length(Students)) {
+
+  # Generate randomised values for student
+  set <- sample(1:8,1)
+  sets <- protset[,set]
+  prots <- data.frame(names = c(names[sets[1]],names[sets[2]],names[sets[3]]),
+                      MW = c(MW[sets[1]],MW[sets[2]],MW[sets[3]]),
+                      A410 = c(A410[sets[1]],A410[sets[2]],A410[sets[3]]))
+  if(set == 1 || set == 5) {
+    doc <- paste0("Dataset15-",sample(1:6,1));
+  } else if(set == 2) {
+    doc <- paste0("Dataset2-",sample(1:5,1));
+  } else if(set == 3 || set == 6) {
+    doc <- paste0("Dataset36-",sample(1:4,1));
+  } else if(set == 4) {
+    doc <- "Dataset4";
+  } else if(set == 7) {
+    doc <- paste0("Dataset7-",sample(1:4,1));
+  } else if(set == 8) {
+    doc <- paste0("Dataset8-",sample(1:4,1));
+  }
+
+  collen <- sample(450:550,1)
+  colrad <- 5
+  colvol <- pi*(collen/10)*((colrad/10)^2)
+  colvoid <- colvol/3
+  theoplat <- sample (400:600,1)
+  m <- -1/log10(150000)
+  c <- 1
+  IEX <- c(sample(13:17,1),2.5,(sample(30:40,1)/100),0,sample(43:51,1),2.5,(sample(30:40,1)/100),0)
+  IEX[4] <- IEX[3]*prots$A410[2]; IEX[8] <- IEX[7]*prots$A410[3]
+  back280 <- sample(400:450,1)/10000
+  back410 <- sample(350:400,1)/10000
+
+  # Run SEC column
+  prots$Kav <- c + (m*log10(prots$MW))
+  prots$Ve <- colvoid + (prots$Kav*(colvol-colvoid))
+  prots$W <- (5.54*(prots$Ve^2)*1000/(collen*theoplat))^0.5
+  SEC <- data.frame(Fractions = c(0:35),
+                    A280 = c(0),
+                    A410 = c(0))
+  SEC2 <- data.frame(Fractions = c(0:35,0:35),
+                     Abs = c(0),
+                     Wavelength = c(0))
+  
+  for (i in 1:36) {
+    SEC$A280[i] <- round((back280 + (sample(20:100,1)/10000) +(((0.3*exp(-(((i+colvoid-2)-prots$Ve[1])^2)/
+              (0.5*(prots$W[1]^2)))) + (0.3*exp(-(((i+colvoid-2)-prots$Ve[2])^2)/(0.5*(prots$W[2]^2))))+
+                (0.3*exp(-(((i+colvoid-2)-prots$Ve[3])^2)/(0.5*(prots$W[3]^2)))))*sample(90:110,1)/100)),3)
+    SEC$A410[i] <- round((back410 + (sample(20:100,1)/10000) +
+                    (((0.3*prots$A410[1]*exp(-(((i+colvoid-2)-prots$Ve[1])^2)/(0.5*(prots$W[1]^2)))) + 
+                    (0.3*prots$A410[2]*exp(-(((i+colvoid-2)-prots$Ve[2])^2)/(0.5*(prots$W[2]^2)))) + 
+                    (0.3*prots$A410[3]*exp(-(((i+colvoid-2)-prots$Ve[3])^2)/(0.5*(prots$W[3]^2)))))*
+                      sample(90:110,1)/100)),3)
+    SEC2$Abs[i] <- SEC$A280[i]; SEC2$Abs[i+36] <- SEC$A410[i]
+    SEC2$Wavelength[i] <- "280 nm"; SEC2$Wavelength[i+36] <- "410 nm"
+  }
+  
+  #Run IEX column
+  IEC <- data.frame(Fractions = c(0:60),
+                    A280 = c(0),
+                    A410 = c(0))
+  IEC2 <- data.frame(Fractions = c(0:60,0:60),
+                     Abs = c(0),
+                     Wavelength = c(0))
+  
+  for (i in 1:61) {
+    IEC$A280[i] <- round((back280 + (sample(20:100,1)/10000) + (((IEX[3]*exp(-(((i-1)-IEX[1])^2)/
+              (0.5*(IEX[2]^2)))) + (IEX[7]*exp(-(((i-1)-IEX[5])^2)/(0.5*(IEX[6]^2)))))*sample(90:110,1)/
+                100)),3)
+    IEC$A410[i] <- round((back410 + (sample(20:100,1)/10000) + (((IEX[4]*exp(-(((i-1)-IEX[1])^2)/
+              (0.5*(IEX[2]^2)))) + (IEX[8]*exp(-(((i-1)-
+              IEX[5])^2)/(0.5*(IEX[6]^2)))))*sample(90:110,1)/100)),3)
+    IEC2$Abs[i] <- IEC$A280[i]; IEC2$Abs[i+61] <- IEC$A410[i]
+    IEC2$Wavelength[i] <- "280 nm"; IEC2$Wavelength[i+61] <- "410 nm"
+  }
+
+  # Generate student files
+  readout <- read_docx(paste0("Start_files/",doc, ".docx"))
+  readout <- readout %>%
+    cursor_begin %>%
+    body_add_par(paste0("Data for student: ", Students[j])) %>%
+    body_add_par("") %>%
+    body_add_par("Data for size exclusion chromatography:", style = "Normal") %>%
+    body_add_par("") %>% 
+    body_add_par(paste0("Column void volume: ", round(colvoid), " mL"), style = "Normal") %>%
+    body_add_par("") %>% 
+    body_add_table(value = SEC, style="Normal Table") %>%
+    body_add_par("") %>%
+    body_add_par("") %>%
+    body_add_par("Data for ion exchange chromatography:") %>%
+    body_add_par("") %>%
+    body_add_table(value = IEC, style="Normal Table") %>%
+    body_add_par("") %>%
+    body_add_par("") %>%
+
+    print(readout, target = paste0("Student_files/", Students[j], ".docx"))
+  
+  # Prepare figures
+  se <- ggplot(data = SEC2, aes(x = Fractions, y=Abs)) 
+  se <- se + geom_path() + geom_point(mapping = aes(size = 2, color = Wavelength)) +
+          labs(y="Absorbance / AU", x="Fraction") 
+  
+  ie <- ggplot(data = IEC2, aes(x = Fractions, y=Abs)) 
+  ie <- ie + geom_path() + geom_point(mapping = aes(size = 2, color = Wavelength)) + 
+          labs(y="Absorbance / AU", x="Fraction")
+  
+  # Generate marker files
+  
+  readout <- read_docx()
+  readout <- readout %>%
+    body_add_par("BIO2090 Laboratory report assignment", style = 'heading 1') %>%
+    body_add_par("") %>%
+    body_add_par(paste0("Results for student: ", Students[j])) %>%
+    body_add_par("") %>%
+    body_add_par(paste0("Correct proteins: ", names[sets[1]], ", ", names[sets[2]], 
+                        ", ", names[sets[3]]), style = "Normal") %>%
+    body_add_par("") %>%
+    body_add_par("Graph for size exclusion chromatography") %>%
+    body_add_par("") %>%
+    body_add_gg(value = se, style = "centered") %>%
+    body_add_par("") %>%
+    body_add_break() %>%
+    body_add_par("Graph for ion exchange chromatography:") %>%
+    body_add_par("") %>%
+    body_add_gg(value = ie, style="centered") %>%
+      
+    print(readout, target = paste0("Lecturer_files/", Students[j], "_answers.docx"))
+  
+}
+
